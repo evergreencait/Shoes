71831a693326c1bac9da24ffc440b9bd9aeca26e
Caitlin Hines and Tammy Dang <caitlin+tammy@>
2017-05-03 10:46:10 -0700

    pass first test

---
+++ ShoesTest.Tests/Models/SaleTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shoes.Models;
+using Xunit;
+
+
+namespace SaleTest.Tests
+{
+    public class ShoeTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var sale = new Sale();
+            sale.ShoeName = "Gucci Shoe";
+
+            //Act
+            var result = sale.ShoeName;
+
+            //Assert
+            Assert.Equal("Gucci Shoe", result);
+        }
+    }
+}
